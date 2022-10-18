7134272fcc2654b1c7dfd3799ea4af506bfedc80
25011988Olga <olga.moseychuk.88@mail.ru>
2022-10-18 07:54:43 +0300

    Выполнила Task36

---
+++ семинар5/Task36/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите кол-во эл-тов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+int result = 0;
+InputArray(array);
+Console.Write($"массив:[{string.Join(", ", array)}] ");
+ReleaseArray(array);
+
+void InputArray(int[] array)
+
+{
+    for(int i = 0; i<array.Length; i++)
+    array[i] = new Random().Next(-100,100);
+}
+ void ReleaseArray(int[] array)
+
+ {
+    for(int i = 0; i<array.Length; i++)
+    {
+    if(i%2==1)
+       result=result+array[i];
+    }
+ }
+Console.Write(result);
